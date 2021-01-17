cdc6aeae3db9a93dfdd5013a9bdb010bca357934
Crackleshine <leejunyansean@gmail.com>
2021-01-17 17:36:46 +0800

    Initial commit

---
+++ appscripts/main.js
@@ -0,0 +1,231 @@
+// main.js
+
+console.log(`yo`);
+
+const months = [
+    "meme complex",
+    "meme complex",
+    "game",
+    "cow", 
+    "sphere", 
+    "cube", 
+    "piece of paper", 
+    "camera", 
+    "person", 
+    "gun",
+    "bomb",
+    "rabbit",
+    "malicious agent",
+    "weapon of Destruction",
+    "EMP",
+    "game",
+    "meme complex",
+    "euclidean object",
+    "work of Art",
+    "God",
+    "source of energy",
+    "knife",
+    "meme complex",
+    "finger",
+    "dog",
+    "ring",
+    "sense of mortality",
+    "means of control",
+    "game",
+    "panopticon",
+    "space",
+    "argument",
+    "object",
+    "act of vengence",
+    "rock",
+    "meme complex",
+    "treasure",
+    "broken heart",
+    "loaf of bread",
+    "toaster",
+    "nuclear warhead",
+    "virus",
+    "type of poision",
+    "frog",
+    "gun",
+    "finger",
+    "dog",
+    "meme complex",
+    "ring",
+    "sense of mortality",
+    "means of control",
+    "panopticon",
+    "space",
+    "argument",
+    "object",
+    "act of vengence",
+    "rock",
+    "treasure",
+    "broken heart",
+    "meme complex",
+    "loaf of bread",
+    "game",
+    "toaster",
+    "nuclear warhead",
+    "virus",
+    "type of poision",
+    "frog",
+    "gun",
+    "cube", 
+    "piece of paper", 
+    "camera", 
+    "person", 
+    "gun",
+    "game",
+    "bomb",
+    "rabbit",
+    "malicious agent",
+    "weapon of Destruction",
+    "EMP",
+    "euclidean object",
+    "work of Art",
+    "God",
+    "source of energy",
+    "knife",
+
+
+];
+
+const random = Math.floor(Math.random() * months.length);
+
+document.getElementById("logs0").innerHTML = 
+"SCP-055 is not a " + months[random] + ".";
+
+document.getElementById("logs1").innerHTML = 
+"SCP-055 is not a " + months[random+1] + ".";
+
+document.getElementById("logs2").innerHTML = 
+"SCP-055 is not a " + months[random+2] + ".";
+
+document.getElementById("logs3").innerHTML = 
+"SCP-055 is not a " + months[random+3] + ".";
+
+document.getElementById("logs4").innerHTML = 
+"SCP-055 is not a " + months[random+4] + ".";
+
+document.getElementById("logs5").innerHTML = 
+"SCP-055 is not a " + months[random+5] + ".";
+
+document.getElementById("logs6").innerHTML = 
+"SCP-055 is not a " + months[random+6] + ".";
+
+document.getElementById("logs7").innerHTML = 
+"SCP-055 is not a " + months[random+7] + ".";
+
+document.getElementById("logs8").innerHTML = 
+"SCP-055 is not a " + months[random+8] + ".";
+
+document.getElementById("logs9").innerHTML = 
+"SCP-055 is not a " + months[random+9] + ".";
+
+document.getElementById("logs10").innerHTML = 
+"SCP-055 is not a " + months[random+10] + ".";
+
+document.getElementById("logs11").innerHTML = 
+"SCP-055 is not a " + months[random+11] + ".";
+
+document.getElementById("logs12").innerHTML = 
+"SCP-055 is not a " + months[random+12] + ".";
+
+document.getElementById("logs13").innerHTML = 
+"SCP-055 is not a " + months[random+13] + ".";
+
+document.getElementById("logs14").innerHTML = 
+"SCP-055 is not a " + months[random+14] + ".";
+
+document.getElementById("logs15").innerHTML = 
+"SCP-055 is not a " + months[random+15] + ".";
+
+document.getElementById("logs16").innerHTML = 
+"SCP-055 is not a " + months[random+16] + ".";
+
+document.getElementById("logs17").innerHTML = 
+"SCP-055 is not a " + months[random+17] + ".";
+
+document.getElementById("logs18").innerHTML = 
+"SCP-055 is not a " + months[random+18] + ".";
+
+document.getElementById("logs19").innerHTML = 
+"SCP-055 is not a " + months[random+19] + ".";
+
+document.getElementById("logs20").innerHTML = 
+"SCP-055 is not a " + months[random+20] + ".";
+
+document.getElementById("logs21").innerHTML = 
+"SCP-055 is not a " + months[random+21] + ".";
+
+document.getElementById("logs22").innerHTML = 
+"SCP-055 is not a " + months[random+22] + ".";
+
+/*document.getElementById("logs23").innerHTML = 
+"SCP-055 is not a " + months[random+23] + ".";
+
+document.getElementById("logs24").innerHTML = 
+"SCP-055 is not a " + months[random+24] + ".";*/
+
+var arr = [];
+while(arr.length < 19){
+    var r = Math.floor(Math.random() * 98) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+console.log(arr);
+
+document.getElementById("num1").innerHTML = 
+"[" + arr[0] + "/100]" 
+
+document.getElementById("num2").innerHTML = 
+"[" + arr[1] + "/100]" 
+
+document.getElementById("num3").innerHTML = 
+"[" + arr[2] + "/100]" 
+
+document.getElementById("num4").innerHTML = 
+"[" + arr[3] + "/100]" 
+
+document.getElementById("num5").innerHTML = 
+"[" + arr[4] + "/100]" 
+
+document.getElementById("num6").innerHTML = 
+"[" + arr[5] + "/100]" 
+
+document.getElementById("num7").innerHTML = 
+"[" + arr[6] + "/100]" 
+
+document.getElementById("num8").innerHTML = 
+"[" + arr[7] + "/100]" 
+
+document.getElementById("num9").innerHTML = 
+"[" + arr[8] + "/100]" 
+
+document.getElementById("num10").innerHTML = 
+"[" + arr[9] + "/100]" 
+
+document.getElementById("num11").innerHTML = 
+"[" + arr[10] + "/100]" 
+
+document.getElementById("num12").innerHTML = 
+"[" + arr[11] + "/100]" 
+
+document.getElementById("num13").innerHTML = 
+"[" + arr[12] + "/100]" 
+
+document.getElementById("num14").innerHTML = 
+"[" + arr[13] + "/100]" 
+
+document.getElementById("num15").innerHTML = 
+"[" + arr[14] + "/100]" 
+
+document.getElementById("num16").innerHTML = 
+"[" + arr[15] + "/100]" 
+
+document.getElementById("num17").innerHTML = 
+"[" + arr[16] + "/100]" 
+
+document.getElementById("num18").innerHTML = 
+"[" + arr[17] + "/100]" 
+
